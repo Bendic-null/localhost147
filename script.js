d8f06590abcb2430082597823e9dd0a7a11f4e19
Bendic-null <miparanumbenedick8@gmail.com>
2025-11-29 15:49:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function changeText() {
+function activate() {
     const msg = document.getElementById("message");
-    msg.textContent = "You clicked the button! Nice.";
+
+    msg.textContent = "System Activated. Welcome!";
+    msg.style.opacity = "1";
+    msg.style.transform = "scale(1.1)";
+
+    setTimeout(() => {
+        msg.style.transform = "scale(1)";
+    }, 300);
 }
